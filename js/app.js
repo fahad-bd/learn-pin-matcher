45758d735351b110e8cc7c90b9ec4d1f01684e4e
Fahad <fahadahammed1@gmail.com>
2023-01-28 19:18:53 +0600

    create key pad with event buble

--- js/app.js
+++ js/app.js
@@ -17,3 +17,21 @@ function generateaPin()
     const pin = getPin();
     document.getElementById('display-pin').value = pin;
 }
+
+
+document.getElementById('key-pad').addEventListener('click', function(even){
+    // console.log(even.target.innerText); //this is even bubble
+    const calInput = document.getElementById('typed-numbers');
+    const number = even.target.innerText;
+    if(isNaN(number)){ // is not a number
+        if(number == 'C'){
+            calInput.value = '';
+        }
+    }
+    else
+    {
+        const previousNumber = calInput.value;
+        const newNumber = previousNumber + number;
+        calInput.value = newNumber;
+    }
+});
